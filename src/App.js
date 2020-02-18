6370432181f027f05fea12facac1c0ea817074e5
PascalTed <monemail@email.com>
2020-02-18 13:11:12 +0100

    Ajouté variable et méthode, pour générer un mot, dans src/App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import './App.css';
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const words = [{word :'ELEPHANT', clue: 'pachyderme'}, {word :'SOLEIL', clue: 'étoile'}, {word :'CERISE', clue: 'fruit'}];
+let chosenWord = 0;
 
 class App extends React.Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            word : this.generateWord(),
+            gameState : "stop",
+            score: 0,
+            chance: 6
+        };
+    }
+    
+    generateWord () {
+
+        if ((chosenWord + 1) <= words.length){
+            return words[chosenWord].word;
+        } else {
+            chosenWord = 0;
+            return words[chosenWord].word;
+        }
+    }
 }
 
 export default App;
