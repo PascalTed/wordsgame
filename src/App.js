9522949bf24792a2b17acd66687441702d0fa52c
PascalTed <monemail@email.com>
2020-02-18 14:01:46 +0100

    Ajouté import DisplayButton dans src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DisplayButton from './DisplayButton';
 import './App.css';
 
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
