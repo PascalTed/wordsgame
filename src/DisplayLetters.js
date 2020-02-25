0970c52ba5cad85cededeba91490c128c735632c
PascalTed <monemail@email.com>
2020-02-25 12:10:07 +0100

    Ajouté balise div dans src/DisplayLetters.js

--- src/DisplayLetters.js
+++ src/DisplayLetters.js
@@ -4,11 +4,13 @@ import './DisplayLetters.css';
 const DisplayLetters = function (props) {
 	
     return (
-        <div className = {`centerLetter ${props.verified}`} onClick= {(e) => props.onClick(e,props.letter)}>
-            <div   className="letter"> 
-                {props.letter}
+        <div>
+            <div className = {`centerLetter ${props.verified}`} onClick= {(e) => props.onClick(e,props.letter)}>
+                <div className="letter"> 
+                    {props.letter}
+                </div>
+
             </div>
-                  
         </div>
     )
 };
