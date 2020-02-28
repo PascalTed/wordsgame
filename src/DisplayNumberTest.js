54710ded40d6c0329d4a4f896ebb31b0282b4dd1
PascalTed <monemail@email.com>
2020-02-28 13:51:44 +0100

    Modifié composant DisplayNumberTest dans src/DisplayNumberTest.js

--- src/DisplayNumberTest.js
+++ src/DisplayNumberTest.js
@@ -6,7 +6,7 @@ const DisplayNumberTest = function (props) {
 	
         
         if (props.gameState === "start" || props.gameState === "initStart" || props.gameState === "addLetter" || props.gameState === "découvrir mot" || props.gameState === "mot découvert" || props.gameState === "mot trouvé") {
-            return (<p>il vous reste {props.chance} essais</p>);
+            return (<p>il vous reste <span id="nbTest">{props.chance}</span> essais</p>);
         }
         else if (props.gameState === "perdu"  || props.gameState === "mot découvert et fini") {
             return (<p>"Vous avez utilisé tous les essais"</p>);
