6d063dcecafa05e01ad622e5c1e745d54d8af677
PascalTed <monemail@email.com>
2020-03-08 12:24:45 +0100

    Ajouté nouveaux mots (dans variable words) dans src/App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-const words = [{word :'ELEPHANT', clue: 'pachyderme'}, {word :'SOLEIL', clue: 'étoile'}, {word :'CERISE', clue: 'fruit'}, {word :'BILBAO', clue: 'ville d\'Espagne'}, {word :'RAMBO', clue: 'film'}, {word :'PARADOXORNIS', clue: 'oiseau'}];
+const words = [{word :'ELEPHANT', clue: 'pachyderme'}, {word :'SOLEIL', clue: 'étoile'}, {word :'CERISE', clue: 'fruit'}, {word :'BILBAO', clue: 'ville d\'Espagne'}, {word :'RAMBO', clue: 'film'}, {word :'PARADOXORNIS', clue: 'oiseau'}, {word :'carotte', clue: 'légume'}, {word :'STEGOSAURE', clue: 'dinosaure'}];
 let chosenWord = 0;
 
 class App extends React.Component {
